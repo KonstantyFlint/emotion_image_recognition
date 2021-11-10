71bdf58197ceeb937c90126b12f17d400de86478
KonstantyFlint <43811569+KonstantyFlint@users.noreply.github.com>
2021-11-10 12:57:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return '<img src="pies niezadowolony.jpg">'
+    return 'asdasdasdasdasd'
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=8080)
