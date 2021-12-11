e334fb3371a582ad2f41a6ddf872774216ec1e47
KonstantyFlint <43811569+KonstantyFlint@users.noreply.github.com>
2021-12-11 01:28:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def homepage():
     return 'asdasdasdasdasd'
 
+@app.route('/uploader', methods = ['POST'])
+def upload_file():
+      f = request.files['file']
+      o = f.read()
+      
+      return str(o);
+
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=8080)
